8f71a7654ce5ac209333ec5c7cff6f4fd5ed249e
James L. Barker <vmjersey@hotmail.com>
2021-01-04 20:20:29 -0500

    Added lstm dataset processing.

--- eidetic_ml/processing.py
+++ eidetic_ml/processing.py
@@ -1,4 +1,49 @@
 import numpy as np
+import sys
+import subprocess
+import pkg_resources
+
+#Packages that can help speed up program
+warn_packages = {'numba'}
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = warn_packages - installed
+
+if missing:
+    print("The following packages are not installed: ", missing)
+
+
+from numba.experimental import jitclass
+from numba import int32, float32    # import the types
+spec = [
+    ('lookback', int32),               # a simple scalar field
+    ('lstmdataset', float32[:]),       # an array field
+    ('dataset', float32[:]),
+]
+
+
+    
+#@jitclass(spec)
+class LSTM_DATA:
+    """ 
+        Basic Class for handling LSTM data.  
+
+
+        transform(lstmdataset,dataset):
+            Transforms a 2d into a 3d dataset of dimension of the type
+            (number of samples,lookback, number of features)
+    """
+
+    def __init__(self,lookback):
+        self.lookback = lookback
+
+    def transform(self,lstmdataset,dataset):
+        num_samples = dataset.shape[0]
+        num_features = dataset.shape[1]
+        for i in range(0, num_samples - self.lookback, 1):
+            sequence = dataset[i:i + self.lookback]
+            lstmdataset[i] = sequence
+
+
 
 class Encoding:
 
